ed4d16626ea3d4445d85dae85390f0a82129c358
“username” <mgoel7@asu.edu>
2023-02-23 16:53:27 -0700

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Manav Goel on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
